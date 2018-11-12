59d8c3fb07113d97a1f1fa2bceffa5cef3948898
Darshan Khot <43519784+deekeh@users.noreply.github.com>
2018-11-12 21:25:46 +0530

    added finding feature

--- prjx.cpp
+++ prjx.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<string.h>
 #include <fstream>
+#include<stdlib.h>
 using namespace std;
 
 bool login()
@@ -17,6 +18,35 @@ bool login()
 	return false;
 }
 
+void find(char data[],int type)
+{
+	ifstream i("dtx.txt");
+	int m = 0;
+	while(!i.eof())
+	{
+		char name[101];
+		i.getline(name,101);
+		char mass[101];
+		i.getline(mass,101);
+		char pin[101];
+		i.getline(pin,101);
+		char stat[101];
+		i.getline(stat,101);
+		//i.getline();
+	
+		switch(type)
+		{
+			case 1:
+				if(strcmp(data,pin) == 0)
+				m += atoi(mass);
+			case 2:
+				if(strcmp(data,name) == 0)
+				m += atoi(mass);
+		}
+	}
+	cout<<m;
+}
+
 main()
 {
 	if(! login()) return 0;
@@ -27,25 +57,32 @@ main()
 	do
 	{
 		cout<<"1. Find using pincode."<<endl<<"2. Find using user-name."<<endl<<"3. Exit."<<endl;
-		cout<<"Enter a choice"<<endl
+		cout<<"Enter a choice"<<endl;
 		cin>>choice;
 		system("cls");
 		//cout<<endl;
 		
-		switch
-	}
-	ifstream i("dtx.txt");
-	
-	//cout<<line;
-	/*char * name;
-	name = strtok(line, "\t");
-	char * mass;
-	mass = strtok(line, "\t");
-	char * pin;
-	pin = strtok(line, "\t");
-	char * stat;
-	stat = strtok(line, "\t");
-	*/
+		switch(choice)
+		{
+			case 1:
+				char pc[7];
+				cout<<"Enter pincode: ";
+				cin>>pc;
+				find(pc,choice);
+				break;
+			case 2:
+				char nm[7];
+				cout<<"Enter user-name: ";
+				cin>>nm;
+				find(nm,choice);
+				break;
+			case 3:
+				break;
+			default:
+				cout<<"Invalid input.";
+		}
+	}while(choice != 3);
+	/*ifstream i("dtx.txt");
 	
 	char name[101];
 	i.getline(name,101);
@@ -62,6 +99,5 @@ main()
 	cout<<"\nMass: "<<mass;
 	cout<<"\nPincode: "<<pin;
 	cout<<"\nStatus: "<<stat;
-	
-	i.close();
+	i.close();*/
 }
